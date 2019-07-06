b92fab403e06332ac47f1a25e3d72ffcaf7978f3
Shaw622 <shaw622.dev@gmail.com>
2019-07-06 16:15:48 +0900

    first commit

---
+++ mensa.py
@@ -0,0 +1,14 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import requests
+import re
+# Mensa の申込状況の確認
+
+page = requests.get("https://mensa.jp/exam/")
+bs = BeautifulSoup(page.content, 'html.parser')
+
+#images = bs.find_all('img', {'src':re.compile('skin/default/image/exam/entry_(out|quota).jpg')})
+images = bs.find_all('img', {'alt':re.compile('(満員|申し込む)')})
+for image in images:
+    print(image['alt'])
+    
